875a5cb80f6873a19b3a2eac3829012e586d7f4b
Grzesiek <grzegorzociepka09@gmail.com>
2020-06-11 15:22:09 +0200

    added components for rest of application

---
+++ src/app/components/form-creator/form-creator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-creator',
+  templateUrl: './form-creator.component.html',
+  styleUrls: ['./form-creator.component.scss']
+})
+export class FormCreatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
