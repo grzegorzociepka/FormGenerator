f02aca5e646d37f5f3fc6d3ea3a83ad7fc8fcfaa
Grzesiek <grzegorzociepka09@gmail.com>
2020-06-13 00:12:13 +0200

    added logic to form-creator component

--- src/app/components/form-creator/form-creator.component.ts
+++ src/app/components/form-creator/form-creator.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
 import {FieldConfig} from '../../model/field-config.interface';
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-form-creator',
@@ -9,13 +10,37 @@ import {FieldConfig} from '../../model/field-config.interface';
 export class FormCreatorComponent implements OnInit {
 
   document: FieldConfig[] = [];
+  form: FormGroup;
+  productFormGroup: FormArray;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  get options() {
+    return this.form.get('options') as FormArray;
+  }
 
   ngOnInit(): void {
       this.document.push({label: 'Submit', name: 'submit', type: 'button'});
+
+      this.form = this.fb.group({
+        name: ['', Validators.required],
+        label: ['', Validators.required],
+        type: ['input', Validators.required],
+        options: this.fb.array([
+          this.fb.control('')
+        ])
+      });
   }
 
+  addOption() {
+    this.options.push(this.fb.control(''));
+  }
 
+  onSubmit() {
+    const newField = {};
+    console.log(this.form.value);
+    this.document.unshift(this.form.value);
+    this.form.reset();
+  }
 
 }
